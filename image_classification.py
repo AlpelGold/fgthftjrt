574e3f1851173f3807cb9491a6a3e51da1187bdf
AlpelGold <130079385+AlpelGold@users.noreply.github.com>
2023-05-10 12:18:42 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -21,7 +21,7 @@ def preprocess_image(img):
 
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    uploaded_file = st.file_uploader(label='кирилл к')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
